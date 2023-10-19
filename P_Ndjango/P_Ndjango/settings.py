483739aac29a5639e012f68dce347ebbd4bda3f1
woni1010011 <romoney@naver.com>
2023-10-19 10:29:57 +0900

    [Feat] settings.py
    - S3 설정
    
    박성원

--- P_Ndjango/P_Ndjango/settings.py
+++ P_Ndjango/P_Ndjango/settings.py
@@ -112,6 +112,24 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "P_Ndjango.wsgi.application"
 
+# AWS S3 셋팅
+AWS_REGION = "ap-northeast-2"
+AWS_STORAGE_BUCKET_NAME = get_secret("BUCKET_NAME")
+AWS_ACCESS_KEY_ID = get_secret("ACCESS_KEY")
+AWS_SECRET_ACCESS_KEY = get_secret("SECRET_ACCESS_KEY")
+
+
+AWS_S3_CUSTOM_DOMAIN = "%s.s3.%s.amazonaws.com" % (AWS_STORAGE_BUCKET_NAME, AWS_REGION)
+print(AWS_S3_CUSTOM_DOMAIN)
+
+# Static Setting
+# python manage.py collectstatic
+STATIC_URL = "http://%s/static/" % AWS_S3_CUSTOM_DOMAIN
+STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+
+# Media Setting
+MEDIA_URL = "http://%s/media/" % AWS_S3_CUSTOM_DOMAIN
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
