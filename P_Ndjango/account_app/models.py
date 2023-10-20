47cd61c84dd3f3c4361b75910798df4da061e9fa
Seonggi <leeseonggi93@gmail.com>
2023-10-20 17:59:21 +0900

    [Feat] account_app, contents_app models 추가
    account_app models:
    User, UserIgrd
    contents_app models:
    Ingredients, Board, Comment

--- P_Ndjango/account_app/models.py
+++ P_Ndjango/account_app/models.py
@@ -1,18 +1,31 @@
 from django.db import models
+from contents_app.models import Ingredients
+
 
 # Create your models here.
 
 class User(models.Model):
-    user_no = models.IntegerField(primary_key=True)
-    user_id = models.CharField(max_length=100, unique=True)
+    user_no = models.BigAutoField(primary_key=True)
+    user_id = models.CharField(max_length=150, unique=True)
     user_password = models.CharField(max_length=200)
-    user_email = models.CharField(max_length=100)
-    user_name = models.CharField(max_length=20)
-    user_nick = models.CharField(max_length=50, blank=True, null=True)
+    user_name = models.CharField(max_length=100)
+    user_email = models.EmailField(max_length=200, unique=True)
+    user_nick = models.CharField(max_length=50, unique=True)
     user_phone = models.CharField(max_length=11)
-    user_address = models.CharField(max_length=200, blank=True, null=True)
+    user_address = models.CharField(max_length=200)
     sub_date = models.DateTimeField(auto_now_add=True)
-    user_point = models.IntegerField(null=True, blank=True)
-    
+    user_point = models.IntegerField(default=0)
+
     def __str__(self):
         return self.user_id
+    
+class UserIgrd(models.Model):
+    ingredients_igrd_no = models.ForeignKey(Ingredients, on_delete=models.CASCADE, related_name="related_user_igrds")
+    user_no = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_no_info")
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user_id_info")
+    igrd_name = models.CharField(max_length=255)
+    user_igrd_date = models.DateField()
+    exp_date = models.DateField()
+
+    def __str__(self):
+        return self.igrd_name
--- P_Ndjango/contents_app/models.py
+++ P_Ndjango/contents_app/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
 # Create your models here.
+
+class Ingredients(models.Model):
+    igrd_no = models.BigAutoField(primary_key=True)  # 주 키로 설정된 bigserial 필드
+    igrd_name = models.CharField(max_length=255, null=False)  # varchar(255) 필드
+    igrd_content = models.TextField(null=True)  # text 필드 (NULL 허용)
+    exp_date = models.DateField(null=False)  # date 필드
+
+    def __str__(self):
+        return self.igrd_name
+    
+class Board(models.Model):
+    board_no = models.BigAutoField(primary_key=True)  # 주 키로 설정된 bigserial 필드
+    post_no = models.IntegerField(null=False)  # int 필드
+    post_title = models.CharField(max_length=255, null=False)  # varchar(255) 필드
+    post_content = models.TextField(null=True)  # text 필드 (NULL 허용)
+    user_nick = models.CharField(max_length=50, null=False)  # varchar(50) 필드
+    post_time = models.DateTimeField(null=False)  # timestamp 필드
+    post_like = models.IntegerField(default=0, null=False)  # int 필드
+
+    def __str__(self):
+        return self.post_title
+
+class Comment(models.Model):
+    post_no = models.ForeignKey(Board, on_delete=models.CASCADE, related_name="comments")  # board 테이블 참조
+    comment_content = models.TextField(null=False)  # text 필드
+    comment_time = models.DateTimeField(null=False)  # timestamp 필드
+
+    def __str__(self):
+        return self.comment_content[:20] + "..."
+
+
+
+
+
