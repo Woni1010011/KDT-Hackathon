9786f7b91ae74f1c8d1afa71ebb239d5ed56860d
KimEuncheol222 <ec5172@naver.com>
2023-10-23 11:17:09 +0900

    [Feat] Img_Search
    Google Vision API를 통해 이미지 객체 판별 후 검색 키워드로 반환
    
    김은철

--- P_Ndjango/contents_app/views.py
+++ P_Ndjango/contents_app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import JsonResponse
 from google.cloud import vision
 from google.cloud.vision_v1 import types
+from django.contrib import messages
 
 # Create your views here.
 def homepage(request):
@@ -13,6 +14,9 @@ def search(request):
 def material_search(request):
     return render(request, 'material_search.html')
 
+# 이미지 검색
+from googletrans import Translator
+
 def img_search(request):
     if request.method == 'POST' and request.FILES['image']:
         image = request.FILES['image']
@@ -20,16 +24,27 @@ def img_search(request):
 
         content = image.read()
         image = types.Image(content=content)
+        # 이미지 객체를 텍스트로 변환
         response = client.object_localization(image=image)
         objects = response.localized_object_annotations
 
         if objects:
             detected_objects = [obj.name for obj in objects]
-            return JsonResponse({'detected_objects': detected_objects})
+            unique_detected_objects = list(set(detected_objects))
+            
+            # 구글 번역 API 한국어로 번역 일일 사용량 제한있음
+            translator = Translator()
+            translated_objects = [translator.translate(obj, src='en', dest='ko').text for obj in unique_detected_objects]
+            
+            # '음식' 및 '패키지 상품' 필터링
+            filtered_objects = [obj for obj in translated_objects if obj not in ['음식', '패키지 상품']]
+            
+            return render(request, 'img_search.html', {'translated_objects': filtered_objects})
         else:
-            return JsonResponse({'error': 'No objects detected in the image'})
+            messages.warning(request, '재료를 찾을 수 없습니다.')
+            return render(request, 'img_search.html')
     else:
-        return render(request, 'img_search.html')    
+        return render(request, 'img_search.html')
 
 def board(request):
     return render(request, 'board.html')
