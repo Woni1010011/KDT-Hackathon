ebf9f975b5c2c11d6cede9b8b6adfa4f0f1e6f1b
AGG-J <hyojooagnes824@gmail.com>
2023-10-25 16:19:48 +0900

    [Feat] login
    로그인 기능 구현
    
    정효주

--- P_Ndjango/account_app/views.py
+++ P_Ndjango/account_app/views.py
@@ -6,44 +6,32 @@ from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 
-# @csrf_exempt
-# def login_view(request):
-#     template = 'registration/login.html'
-
-#     if request.method == 'POST':
-#         inputId = request.POST.get('id', '')  # 사용자가 입력한 ID
-#         inputPassword = request.POST.get('password', '')  # 사용자가 입력한 비밀번호
-
-#         try:
-#             user = User.objects.get(user_id=inputId)  # 입력한 ID로 사용자 조회
-#             if user.user_password == inputPassword:
-#                 # 로그인에 성공하면 세션에 사용자 ID 저장
-#                 request.session['user_id'] = user.user_id
-#                 return redirect('main')  # 로그인 성공 시 리디렉션할 페이지로 이동
-#             else:
-#                 context = {
-#                     "result": "비밀번호가 틀렸습니다"
-#                 }
-#         except User.DoesNotExist:
-#             context = {
-#                 "result": "존재하지 않는 id입니다"
-#             }
-#     else:
-#         context = {}  # GET 요청일 경우, 빈 컨텍스트 전달
-
-#     return render(request, template, context)
-
+@csrf_exempt
 def login_view(request):
+    template = 'registration/login.html'
+
     if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        user = auth.authenticate(request, username=username, password=password)
-        if user is not None:
-            auth.login(request, user)
-            return redirect('login')
-        else:
-            return render(request, 'registration/login.html'), {'error':'ID와 비밀번호를 확인해 주세요.'}
-    return render(request, 'registration/login.html')
+        inputId = request.POST.get('id', '')  # 사용자가 입력한 ID
+        inputPassword = request.POST.get('password', '')  # 사용자가 입력한 비밀번호
+
+        try:
+            user = User.objects.get(user_id=inputId)  # 입력한 ID로 사용자 조회
+            if user.user_password == inputPassword:
+                # 로그인에 성공하면 세션에 사용자 ID 저장
+                request.session['user_id'] = user.user_id
+                return redirect('main')  # 로그인 성공 시 리디렉션할 페이지로 이동
+            else:
+                context = {
+                    "result": "비밀번호가 틀렸습니다"
+                }
+        except User.DoesNotExist:
+            context = {
+                "result": "존재하지 않는 id입니다"
+            }
+    else:
+        context = {}  # GET 요청일 경우, 빈 컨텍스트 전달
+
+    return render(request, template, context)
 
 def social_login_view(request):
     return render(request, 'registration/login.html')
