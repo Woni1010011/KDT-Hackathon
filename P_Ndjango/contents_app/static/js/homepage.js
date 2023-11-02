c4068f5fabec700449a6e90e1193fe6c48c5fe8b
woni1010011 <romoney@naver.com>
2023-11-02 14:15:00 +0900

    [Feat] homepage.js
    - 코드 오류 수정
    
    박성원

--- P_Ndjango/contents_app/static/js/homepage.js
+++ P_Ndjango/contents_app/static/js/homepage.js
@@ -14,34 +14,28 @@ document.addEventListener("DOMContentLoaded", function() {
     var categories_contents_area = document.getElementsByClassName("categories_contents_area")[0];
 
     for (var i = 0; i < imageUrls.length; i++) {
-        (function(i) { // 즉시 실행 함수를 추가
-            var category_content = document.createElement('div');
-            category_content.className = 'categories_content';
+        var category_content = document.createElement('div');
+        category_content.className = 'categories_content';
 
-            var category_img = document.createElement('div');
-            category_img.className = 'category_img';
+        var category_img = document.createElement('div');
+        category_img.className = 'category_img';
 
-            var img = document.createElement('img');
-            img.src = imageUrls[i];
-            img.alt = '';
-            
-            // 이미지를 클릭하면 새로운 URL로 리디렉션하는 이벤트 리스너를 추가합니다.
-            img.addEventListener('click', function() {
-                window.location.href = "http://127.0.0.1:8000/categories_result?q=" + categories[i];
-            });
+        var img = document.createElement('img');
+        img.src = imageUrls[i];
+        img.alt = '';
 
-            category_img.appendChild(img);
+        category_img.appendChild(img);
 
-            var category_title = document.createElement('div');
-            category_title.className = 'category_title';
+        var category_title = document.createElement('div');
+        category_title.className = 'category_title';
 
-            var p = document.createElement('p');
-            p.textContent = categories[i];
+        var p = document.createElement('p');
+        p.textContent = categories[i];
 
-            category_title.appendChild(p);
+        category_title.appendChild(p);
 
-            category_content.appendChild(category_img);
-            category_content.appendChild(category_title);
+        category_content.appendChild(category_img);
+        category_content.appendChild(category_title);
 
         categories_contents_area.appendChild(category_content);
 
